0e9ae4ec58741986799c28129f238e3c0918bf20
preetha sb <preetha.sb@walmartlabs.com>
2023-06-08 20:35:25 +0530

    added cart-page

--- src/components/context/CartContext.js
+++ src/components/context/CartContext.js
@@ -6,15 +6,34 @@ export const CartProvider = ({ children }) => {
   const [cartItems, setCartItems] = useState([]);
 
   const addToCart = (item) => {
-    setCartItems((prevItems) => [...prevItems, item]);
-  };
+    const existingItem = cartItems.find((cartItem) => cartItem.id === item.id);
+    if (existingItem) {
+      const updatedItem = { ...existingItem, quantity: existingItem.quantity + 1 };
+      setCartItems((prevItems) =>
+        prevItems.map((cartItem) => (cartItem.id === item.id ? updatedItem : cartItem))
+      );
+    } else {
+      const newItem = { ...item, quantity: 1 };
+      setCartItems((prevItems) => [...prevItems, newItem]);
+    }
 
+  };
+  
   const removeFromCart = (item) => {
     setCartItems((prevItems) => prevItems.filter((i) => i.id !== item.id));
   };
-
+  const updateQuantity = (itemId, newQuantity) => {
+    const updatedCartItems = cartItems.map((item) => {
+      if (item.productId === itemId) {
+        return { ...item, quantity: newQuantity };
+      }
+      return item;
+    });
+    setCartItems(updatedCartItems);
+  };
+  
   return (
-    <CartContext.Provider value={{ cartItems, addToCart, removeFromCart }}>
+    <CartContext.Provider value={{ cartItems, addToCart, removeFromCart ,updateQuantity}}>
       {children}
     </CartContext.Provider>
   );
